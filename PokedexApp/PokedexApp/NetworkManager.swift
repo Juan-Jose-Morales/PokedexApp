8bdefc2710c8f33f9b39dcd4546b289853b60e22
JuanJose <jhonajose@hotmail.es>
2024-10-22 22:28:21 +0200

    The data and network call function has been completed

---
+++ PokedexApp/PokedexApp/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  PokedexApp
+//
+//  Created by Juan jose Morales on 22/10/24.
+//
+
+import Foundation
---
+++ PokedexApp/PokedexApp/PokemonModel.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonModel.swift
+//  PokedexApp
+//
+//  Created by Juan jose Morales on 22/10/24.
+//
+
+import Foundation
